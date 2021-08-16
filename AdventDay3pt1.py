6ba40a0026a57ae73534fb26ae7a5f0d4ccb0ea2
bludbath <bluttier@gmail.com>
2021-08-16 14:46:02 -0600

    Update AdventDay3pt1.py
    Changing for loop into a while loop. The -1 in the if statement isn't pretty

--- AdventDay3pt1.py
+++ AdventDay3pt1.py
@@ -1,17 +1,18 @@
 with open('Day3input.txt') as f:
-    lines = f.readlines()
-    mountain = [line.strip() for line in lines]
+    map = f.readlines()
+    map = [ line.strip() for line in map]
 
-treecount = 0
-ride = 0
-drop = 0
 
-for i in range(len(mountain) - 1):
-    ride += 1
-    drop += 3
-    tree = mountain[ride][drop % len(mountain[ride])]
+treecounter = 0
+hill, slope = 0,0
 
-    if tree == '#':
-        treecount += 1
 
-print('treecount', treecount)
+while hill +1 < len(map):
+    hill += 1
+    slope += 3
+
+    mountain = map[hill][slope % len(map[hill])]
+    if mountain == '#':
+            treecounter += 1
+
+print(treecounter)
