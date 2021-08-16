289c889a9a3a335a8b710d7292c6269d7baf2f1d
bludbath <bluttier@gmail.com>
2021-08-16 14:49:21 -0600

    Update AdventDay3pt1.py

--- AdventDay3pt1.py
+++ AdventDay3pt1.py
@@ -7,12 +7,14 @@ treecounter = 0
 hill, slope = 0,0
 
 
-while hill +1 < len(map):
-    hill += 1
-    slope += 3
+while hill < len(map):
+
 
     mountain = map[hill][slope % len(map[hill])]
     if mountain == '#':
             treecounter += 1
 
+    hill += 1
+    slope += 3
+    
 print(treecounter)
